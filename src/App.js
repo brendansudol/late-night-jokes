0b68b5d4b1a14a59e790cb330e761220c29d1ba2
brendan sudol <brendansudol@gmail.com>
2019-04-01 22:06:33 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+const HOST_OPTIONS = ['--', 'conan', 'colbert']
+const YEAR_OPTIONS = ['--', '2010', '2011']
 
 class App extends Component {
+  state = {
+    jokes: [],
+    query: '',
+    host: '--',
+    year: '--',
+    isLoading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+    const response = await fetch('https://late-night-jokes-api.herokuapp.com/')
+    const data = await response.json()
+    this.setState({ jokes: data.results, isLoading: false })
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
   render() {
+    const { host, jokes, query, isLoading } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="search" name="query" value={query} onChange={this.handleChange} />
+          <select name="host" value={host} onChange={this.handleChange}>
+            {HOST_OPTIONS.map((value, i) => (
+              <option key={i}>{value}</option>
+            ))}
+          </select>
+          <button type="submit">submit</button>
+        </form>
+        {jokes.length === 0 ? (
+          <p>{isLoading ? 'Loading...' : 'No jokes :('}</p>
+        ) : (
+          <div style={{ opacity: isLoading ? 0.5 : 1 }}>
+            {jokes.map(joke => (
+              <p key={joke.id}>{joke.text}</p>
+            ))}
+          </div>
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
