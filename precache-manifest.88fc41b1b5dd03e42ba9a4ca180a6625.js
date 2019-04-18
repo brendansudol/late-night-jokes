ed35cda5a2cf228f6ea262a29546f6a1ab09ad6e
brendan sudol <brendansudol@gmail.com>
2019-04-17 22:14:03 -0400

    Updates

---
+++ precache-manifest.88fc41b1b5dd03e42ba9a4ca180a6625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "775b0f4e029db284fe05",
+    "url": "/static/js/main.38105b02.chunk.js"
+  },
+  {
+    "revision": "38aa5690af856fa9293e",
+    "url": "/static/js/2.772014db.chunk.js"
+  },
+  {
+    "revision": "775b0f4e029db284fe05",
+    "url": "/static/css/main.2a5b4246.chunk.css"
+  },
+  {
+    "revision": "38aa5690af856fa9293e",
+    "url": "/static/css/2.64ac3f03.chunk.css"
+  },
+  {
+    "revision": "163ff084b71e99864436042b672568ed",
+    "url": "/index.html"
+  }
+];
